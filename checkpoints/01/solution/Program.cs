0327cca69ca963a7870576bfc32ad133f45b5317
Tom Bell <tjbell@gmail.com>
2021-12-09 13:06:28 -0700

    new checkpoint

---
+++ checkpoints/01/solution/Program.cs
@@ -0,0 +1,19 @@
+System.String firstString = new System.String("first");
+
+String secondString = "second";
+
+string thirdString = "third";
+
+var forthString = "forth";
+var anotherForthString = "forth";
+forthString = null;
+forthString = "";
+forthString = string.Empty;
+
+const string fifthString = "fifth";
+//fifthString = null;
+
+var withQuotesOne = "He said \"hello\"";
+var pathOne = "c:\\MyFolder";
+var pathTwo = @"c:\MyFolder";
+var withQuotesTwo = @"He said ""hello""";
